7a5e154f71362ca3c98ef3505951d529c1810e84
Jackie Weng <jackie@jemeraldsoft.co.nz>
2022-02-25 13:18:23 +1300

    Adopt webpack public-path

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ if (env) {
 
 const main = ['./src/site.js']
 const common = ['./src/common.js']
+
+const ASSET_PATH = process.env.ASSET_PATH || '/';
+
 let devtool
 
 if (isDev) {
@@ -45,6 +48,7 @@ const plugins = [
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     'process.env.ENABLE_GOOGLE_AUTH': JSON.stringify(process.env.ENABLE_GOOGLE_AUTH),
     'process.env.GTM_ID': JSON.stringify(process.env.GTM_ID),
+    'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
   }),
 ]
 
@@ -60,7 +64,7 @@ module.exports = {
 
   output: {
     path: buildPath,
-    publicPath: '/',
+    publicPath: ASSET_PATH,
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'images/[name][ext]',
   },
