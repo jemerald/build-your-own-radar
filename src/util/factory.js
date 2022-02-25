5bb74537566953d4d1b381cff610342854d8fa50
Jackie Weng <jackie@jemeraldsoft.co.nz>
2022-02-25 20:23:21 +1300

    Use relative path for images

--- src/util/factory.js
+++ src/util/factory.js
@@ -288,7 +288,7 @@ function plotLogo (content) {
   content
     .append('div')
     .attr('class', 'input-sheet__logo')
-    .html('<a href="https://www.thoughtworks.com"><img src="/images/tw-logo.png" / ></a>')
+    .html('<a href="https://www.thoughtworks.com"><img src="images/tw-logo.png" / ></a>')
 }
 
 function plotFooter (content) {
